b222cdf82162ab2d4ec960c8817782f00cf05f08
string2340set <ritika.singh1_cs18@gla.ac.in>
2020-02-24 22:55:16 +0530

    init commit

---
+++ src/ADT/LinkedListADT.java
@@ -0,0 +1,11 @@
+package ADT;
+
+public interface LinkedListADT<Person> {
+    boolean add(Person details);
+
+    boolean delete(int index);
+
+    void search(String person);
+
+    void viewALLDetails();
+}
